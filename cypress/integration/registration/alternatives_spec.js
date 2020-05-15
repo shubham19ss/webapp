48c885b0d83f42ae3aff936ee688e928cca8bcad
santiagonoguez <santiago.arriaga9@gmail.com>
2020-05-15 11:23:26 -0500

    check page behavior

---
+++ cypress/integration/registration/alternatives_spec.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef, max-statements, no-magic-numbers */
+
+const { START_URL } = require( '../../support/config' )
+
+describe('registration / information', () => {
+
+  beforeEach(() => {
+    cy.visit( START_URL )
+
+    cy.get( 'button' ).contains( 'Start creating your account' )
+    .click()
+
+    cy.get( 'h3' ).should( 'contain', 'Terms of service' )
+    cy.get( 'input[name="termsOfService"]' ).click()
+    cy.get( 'button' ).contains( 'Next' ).should( 'be.enabled' )
+    .click()
+  })
+
+  it('should display the alternatives page', () => {
+    cy.get( 'h3' ).should( 'contain', 'Choose the alternative' )
+
+    cy.get( 'input[value="helper"]' ).should( 'not.be.checked' )
+    cy.get( 'input[value="inneed"]' ).should( 'not.be.checked' )
+
+    cy.get( 'button' ).contains( 'Next' ).should( 'be.disabled' )
+  })
+
+  it('should enable next button when volunteer role is selected', () => {
+    cy.get( 'h3' ).should( 'contain', 'Choose the alternative' )
+
+    cy.get( 'input[value="helper"]' ).click()
+
+    cy.get( 'button' ).contains( 'Next' ).should( 'be.enabled' )
+  })
+
+  it('should enable next button when individual role is selected', () => {
+    cy.get( 'h3' ).should( 'contain', 'Choose the alternative' )
+
+    cy.get( 'input[value="inneed"]' ).click()
+
+    cy.get( 'button' ).contains( 'Next' ).should( 'be.enabled' )
+  })
+
+})
