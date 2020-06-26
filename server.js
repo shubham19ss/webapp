698253e41c0d305c52ef9b1cb04cff8276fece00
shubham19ss <35145373+shubham19ss@users.noreply.github.com>
2020-06-26 18:18:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const ip = process.env.IP ||'0.0.0.0';
-const port = process.env.PORT || 8080;
+const ip = process.env.IP ||'127.0.0.1';
+const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
 app.listen(port, ip, function() {
